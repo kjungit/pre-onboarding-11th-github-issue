a3a5b7fcb6cd310f2dfb23517296d97b9d1512b6
kjun <kjun_all@naver.com>
2023-07-13 09:30:17 +0900

    Feat: ContextProvider 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./router/Router";
+import IssueProvider from "./contexts/issueContext";
 
 function App() {
   return (
-    <>
+    <IssueProvider>
       <RouterProvider router={router} />
-    </>
+    </IssueProvider>
   );
 }
 
