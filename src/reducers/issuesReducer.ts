cb74f564d4d767122fc31bf323f82de08c8fd637
kjun <kjun_all@naver.com>
2023-07-13 09:35:48 +0900

    Feat: 이슈리스트 리듀서 로직 구현

---
+++ src/reducers/issuesReducer.ts
@@ -0,0 +1,38 @@
+import { IIssueResponseProps } from "../apis/services/issues";
+
+export interface IIssuesState {
+  loading: boolean;
+  data: any;
+  error: any;
+}
+
+export type IIssuesAction =
+  | { type: "LOADING" }
+  | { type: "SUCCESS"; data: any }
+  | { type: "ERROR"; error: any };
+
+export function issuesReducer(state: IIssuesState, action: IIssuesAction) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data as IIssueResponseProps,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        error: action.error,
+      };
+    default:
+      throw new Error(`처리되지 않은 타입입니다. `);
+  }
+}
