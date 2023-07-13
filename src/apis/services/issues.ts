6418e2dd59371c17668497c717056c53c78bd0dd
kjun <kjun_all@naver.com>
2023-07-13 09:30:42 +0900

    Feat: API 로직 추가

--- src/apis/services/issues.ts
+++ src/apis/services/issues.ts
@@ -1,6 +1,9 @@
 import { axiosInstancex } from "../axios";
 
-interface IIssueResponseProps {
+export interface IIssueResponseProps {
+  data: IIssueItemProps[];
+}
+export interface IIssueItemProps {
   id: number;
   title: string;
   body: string;
@@ -27,9 +30,26 @@ interface ILabelsProps {
 export const getIssues = async () => {
   try {
     const response = await axiosInstancex.get<IIssueResponseProps>(
-      "repos/facebook/react/issues"
+      "repos/facebook/react/issues",
+      {
+        params: {
+          q: "is:open is:issue",
+          sort: "comments-desc",
+        },
+      }
     );
     return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getDetailIssues = async ({ id }: { id: number }) => {
+  try {
+    const response = await axiosInstancex.get<IIssueItemProps>(
+      `repos/facebook/react/issues/${id}`
+    );
+    return response.data;
   } catch (error) {
     console.error(error);
   }
